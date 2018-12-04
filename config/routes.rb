6b12381e9983a983911e60dcfe1d3a625962d8ad
Louis Mayaud <louis.myd@gmail.com>
2018-12-04 17:19:33 +0100

    sample controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'receptions/index'
-  get 'receptions/new'
-  get 'receptions/create'
-  get 'receptions/edit'
-  get 'receptions/update'
-  get 'samples/index'
-  get 'samples/new'
-  get 'samples/create'
+  resources :samples, only: [:index, :edit, :update]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
