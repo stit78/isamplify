949824a95cd44f1f3b4a56e71c996548a7cc9350
rodrigue de poix <rodriguedepoix@hotmail.com>
2018-12-04 14:35:22 +0100

    controller sample

---
+++ app/controllers/samples_controller.rb
@@ -0,0 +1,33 @@
+class SamplesController < ApplicationController
+  def index
+    @samples = Sample.all
+  end
+
+  def show
+    @sample = Sample.find(params[:id])
+  end
+
+  def new
+    @coffeelot = CoffeeLot.find(params[:id])
+    @sample = Sample.new
+    # authorize @coffeelot
+    # authorize @sample
+  end
+
+  def create
+    @sample = Sample.find(params[:id])
+    @sample.coffee_lot = coffeelot
+    @sample.role = "Trader"
+    if @sample.save
+      redirect_to sample_path(@sample)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:sample).permit(:stage, :sweetness, :acidity, :clean, :status)
+  end
+end
