07769933c72de97788457ce2119066188b849fe1
rodrigue de poix <rodriguedepoix@hotmail.com>
2018-12-04 16:31:26 +0100

    pleins de trucs un peu random

--- app/controllers/receptions_controller.rb
+++ app/controllers/receptions_controller.rb
@@ -1,10 +1,34 @@
 class ReceptionsController < ApplicationController
   def index
+    @samples = Sample.all
   end
 
-  def edit
+  def show
+    @sample = Sample.find(params[:id])
   end
 
-  def update
+  def new
+    @sample = Sample.new
+    # authorize @coffeelot
+    # authorize @sample
+  end
+
+  def create
+    @sample = Sample.find(params[:id])
+    @sample.trader = current_user.role
+    @sample.status = "Received"
+    @sample.stage = "Offer Sample"
+
+    if @sample.save
+      redirect_to sample_path(@sample)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:sample).permit(:stage, :sweetness, :acidity, :clean, :status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'receptions/index'
-  get 'receptions/new'
-  get 'receptions/create'
-  get 'receptions/edit'
-  get 'receptions/update'
-  get 'samples/index'
-  get 'samples/new'
-  get 'samples/create'
   devise_for :users
   root to: 'pages#home'
+  resources :samples
+  resources :receptions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
