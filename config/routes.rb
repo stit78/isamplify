f0905283958cc7f28df210ad61f3439a4d85f252
Louis Mayaud <louis.myd@gmail.com>
2018-12-06 14:48:05 +0100

    seed and pdf etiquette

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   end
 
   resources :receptions
-  resources :etiquettes, only: [:new, :index]
+  resources :etiquettes, only: [:create, :index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
