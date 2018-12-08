b6c4a93f52e45d6b7fceb63bd54d1471ef9df34a
thibaut salle <thibaut.m.salle@gmail.com>
2018-12-08 15:03:26 +0100

    weekend modification

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,15 @@ class PagesController < ApplicationController
 
   def home
     @samples = Sample.all.order(stage: :desc)
+    @coffee_lot = CoffeeLot.all
+    @quantity = 0
+    @quantity = @coffee_lot.map { |lot| lot.quantity }
+
     @pending_sample_count = Sample.where(status: "pending").count
     @received_sample_count = Sample.where(status: "received").count
     @tested_sample_count = Sample.where(status: "tested").count
     @labelled_sample_count = Sample.where(status: "labelled").count
+
   end
 
   def test
