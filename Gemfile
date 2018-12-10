7e0fc5c8149856c43c6ca574436f471d68ba8e75
rodrigue de poix <rodriguedepoix@hotmail.com>
2018-12-10 10:22:26 +0100

    emailing dev need test

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'webpacker'
 gem 'wicked_pdf'
 gem 'wkhtmltopdf-binary'
 gem "letter_opener", group: :development
+gem 'postmark-rails'
+
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,12 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+
+
+  config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.postmark_settings   = { api_key: ENV['a09e55eb-e346-44ea-aafd-c7bdb3107833'] }
+  config.action_mailer.default_url_options = { host: "isamplify.com" }
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
