6d718a8489951c516dcdbb495d58555b92e0255f
Louis Mayaud <louis.myd@gmail.com>
2018-12-11 19:05:40 +0100

    end of day

--- app/javascript/components/closeModal.js
+++ app/javascript/components/closeModal.js
@@ -2,7 +2,7 @@ const closeModal = () => {
   let modalButton = document.querySelector(".modal_save")
   modalButton.addEventListener('click', (event) => {
     let modalContainer = document.querySelector(".modal");
-    modalContainer.modal('toggle')
+    modalContainer.modal('toggle');
     });
 };
 
