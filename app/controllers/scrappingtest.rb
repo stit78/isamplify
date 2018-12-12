801551902ea92af5ee503bc76c7b4104c7358855
sacha korsec <korsecsacha@gmail.com>
2018-12-12 16:39:47 +0100

    scrapping in progress

--- app/controllers/scrappingtest.rb
+++ app/controllers/scrappingtest.rb
@@ -25,16 +25,21 @@ html_doc.search('.blog-post .blog-featured-bg-image').each do |element|
   picture_scrap << element.attribute('style').value
 end
 
-picture_url = []
+picture_urls = []
 picture_scrap.each do |element|
-  if element == nil
-    picture_url << ["https://5rewou8vp5-flywheel.netdna-ssl.com/wp-content/uploads/2014/08/coffee-tree.jpg", "http://www.motifcoffee.com/sites/default/files/paragraph-images/2017-09/About-Coffee-Plant_546634210.jpg", "http://cdn2.hubspot.net/hub/57382/file-14822877-jpg/images/loved_import_coffee_beans.jpg?t=1464200267579&width=362&height=319"].sample
+  if element.nil?
+    picture_urls << ["https://5rewou8vp5-flywheel.netdna-ssl.com/wp-content/uploads/2014/08/coffee-tree.jpg", "http://www.motifcoffee.com/sites/default/files/paragraph-images/2017-09/About-Coffee-Plant_546634210.jpg", "http://cdn2.hubspot.net/hub/57382/file-14822877-jpg/images/loved_import_coffee_beans.jpg?t=1464200267579&width=362&height=319"].sample
   else
-    picture_url = element.match("https?:\/\/.*\.(?:png|jpg)")
-    picture_url << picture_url
+    matcher = element.match("https?:\/\/.*\.(?:png|jpg)")
+    if matcher.nil?
+      picture_url = ["https://5rewou8vp5-flywheel.netdna-ssl.com/wp-content/uploads/2014/08/coffee-tree.jpg", "http://www.motifcoffee.com/sites/default/files/paragraph-images/2017-09/About-Coffee-Plant_546634210.jpg", "http://cdn2.hubspot.net/hub/57382/file-14822877-jpg/images/loved_import_coffee_beans.jpg?t=1464200267579&width=362&height=319"].sample
+    else
+      picture_url = matcher[0]
+    end
+    picture_urls << picture_url
   end
 end
-p picture_url
+p picture_urls
 
 text = []
 html_doc.search('.home-excerpt').each do |element|
